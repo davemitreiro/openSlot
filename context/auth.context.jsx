e82159d708dfdf681de6c0844501e35353140ec5
davemitreiro <davemitreiro@gmail.com>
2024-08-29 17:08:11 +0100

    k

---
+++ context/auth.context.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useEffect } from "react";
+import axios from "axios";
+
+// Initializing context
+const AuthContext = React.createContext();
+
+const API_URL = "https://openslot-server.adaptable.app";
