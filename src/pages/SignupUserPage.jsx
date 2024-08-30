9d23b5c65a97f142d5da342c8c2f64baa59a0ed6
davemitreiro <davemitreiro@gmail.com>
2024-08-30 14:31:11 +0100

    a

--- src/pages/SignupUserPage.jsx
+++ src/pages/SignupUserPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-export default function Signup({ createLocation }) {
+export default function Signup({ createUser }) {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
